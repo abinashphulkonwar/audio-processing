bbbee513ba858d55bae2b9e02d4d6cdd59d7fccf
abinash <abinashphulkonwar1@gmail.com>
2023-05-26 20:19:25 +0530

    add pointer objects

--- main.cpp
+++ main.cpp
@@ -4,21 +4,36 @@
 
 int main()
 {
-    std::cout << "data" << std::endl;
+    std::string data[] = {"🚀", "🎢", "🎡", "🎠"};
+    const int length = sizeof(data) / sizeof(std::string);
+    std::cout << length << " " << sizeof(std::string) << std::endl;
+    for (int i = 0; i < length; i++)
+    {
+        std::cout << data[i]
+                  << std::endl;
+    }
+
+    std::cout
+        << "data" << std::endl;
     int val = 93748743;
     int *const ref = &val;
-
+    if (&val == ref)
+    {
+        std::cout << " 🚀" << std::endl;
+    }
     std::cout << ref << std::endl;
     *ref = 9837734;
-    std::cout << *ref << std::endl;
+    std::cout << *ref << " " << val << std::endl;
     {
-        Render renderScreen;
+        Render *renderScreen = new Render;
 
-        renderScreen.start();
+        renderScreen->start();
         std::string val = "🎢";
-        renderScreen.start(val);
-        renderScreen.start();
-        renderScreen.start();
+        renderScreen->start(val);
+        renderScreen->start();
+        renderScreen->start();
+        if (renderScreen != NULL)
+            delete renderScreen;
     }
     {
         Render renderScreen(1);
@@ -28,5 +43,6 @@ int main()
         renderScreen.start();
         renderScreen.start();
     }
+
     return 0;
 }
